5b99d0a4444c8018f844775090084274c88212f4
Jose Maia <maianeto2014@gmail.com>
2025-03-17 20:49:06 -0300

    just a test

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
         Seu navegador não suporta o vídeo.
       </video>
       <div className="banner-content">
-        <h1>Bem-vindo a Tech World!</h1>
+        <h1>Bem-vindos a Tech World!</h1>
         <p>Encontre os melhores produtos do mundo com descontos especiais.</p>
         <a href="#produtos" className="cta-button">Confira Agora</a>
       </div>
