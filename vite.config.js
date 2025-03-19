d2026ff475147579b2d4ec230e871e05d790cd1d
Jose Maia <maianeto2014@gmail.com>
2025-03-18 21:20:20 -0300

    Changed config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/Tech-World/',  
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://projetotestespringboot.onrender.com/users', // Your Spring Boot backend URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix
+      },
+    },
+  },
+});
